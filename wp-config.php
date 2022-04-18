f920b2bb4ee5870a358ec3a3f7458925ec84c0c7
naman-jnext <naman.jnext@gmail.com>
2022-04-18 17:37:02 +0530

    [TODO] Initial Update

--- wp-config.php
+++ wp-config.php
@@ -7,6 +7,25 @@
  * @package ddevapp
  */
 
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'fr1Ndcy7pT' );
+
+/** Database username */
+define( 'DB_USER', 'fr1Ndcy7pT' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'GV1FYqlguv' );
+
+/** Database hostname */
+define( 'DB_HOST', 'remotemysql.com' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
 /** Authentication Unique Keys and Salts. */
 define( 'AUTH_KEY', 'IeqAhlgGCimYNtNodaieGwNbewYmiHBKSKiCyZJuBpJQrYjjQsDuKcKqPWipmuMl' );
 define( 'SECURE_AUTH_KEY', 'uwjDxIChwDgvauxFIzoFitevHldVxZwchpPSTAvoyeRUXnwCQvVZpOBNvSUXlKdT' );
